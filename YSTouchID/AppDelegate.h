0997d69e2d4e7bf03f51215f33a930d9ca415188
TracyYanghandsome <2224202043@qq.com>
2016-11-03 14:32:39 +0800

    Initial Commit

---
+++ YSTouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YSTouchID
+//
+//  Created by yanghandsome on 2016/11/3.
+//  Copyright © 2016年 yanghandsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YSTouchID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSTouchID
+//
+//  Created by yanghandsome on 2016/11/3.
+//  Copyright © 2016年 yanghandsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
